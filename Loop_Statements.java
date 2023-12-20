1c6f1dd2aa338ad101a53dd573d853bff401066f
Shubham Maurya <114727104+Shubhamprogrammar@users.noreply.github.com>
2023-12-20 22:59:14 +0530

    Add files via upload

---
+++ Loop_Statements.java
@@ -0,0 +1,46 @@
+public class Loop_Statements {
+    public static void main(String[] args) {
+        // While Loop
+        int a=1;
+        while (a<=10) {
+            System.out.println(a+" Shubham");
+            a=a+1;
+        }
+        System.out.println("While Loop Ended successfully!!");
+
+        // Do-while Loop
+        int i=100;
+        do {
+            System.out.println(i);
+            i +=1;
+        } while (i<10);
+
+        // For Loop
+        for(int b=5;b>0;b--){
+            System.out.println(b);
+        }
+
+        // Break and Continue
+        int n=1;
+        do {
+            System.out.println(n);
+            if (n==5) {
+                System.out.println("Loop ends here");
+                break;
+            }n++;
+        } while (n<10);
+
+        for(int c=0;c<10;c++){
+            if (c>=5 && c<8) {
+                continue;
+            }
+            System.out.println(c);
+        }
+    }
+}
+
+
+// Practice
+// Q.1. Write a program to sum first n natural numbers (n taken from input)
+// Q.2. Write a program to print multiplication table of given numbers using while loop(n=13)
+// Q.3. Write a program to print the factorial of given number using for loop
